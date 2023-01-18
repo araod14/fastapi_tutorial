3510b0c3e0c0b448bc8de6d9a3d3f2c3ba2103eb
Daniel Arao <araodaniel14@gmail.com>
2023-01-18 14:52:47 -0400

    creada databse

--- config/db.py
+++ config/db.py
@@ -1,1 +1,12 @@
 ## Database
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.orm import sessionmaker
+
+
+engine = create_engine('postgresql://danel149:danel@localhost/fast_tuto')
+
+session_local = sessionmaker(engine)
+
+conn = engine.connect()
+
+meta = MetaData()
--- models/person.py
+++ models/person.py
@@ -1,1 +1,17 @@
 ##basedata table
+
+from sqlalchemy import Table, Column
+from sqlalchemy import Integer, String
+from config.db import meta , engine
+
+
+persons = Table(
+    'persons',
+    meta,
+    Column('id', Integer, primary_key=True),
+    Column('first_name', String(255)),
+    Column('last_name',String(255)),
+    Column('age',int(50))
+)
+
+meta.create_all(engine)
