5a4f37e86b6f103c30d36998c0d004ee4dd28ba0
Daniel Arao <araodaniel14@gmail.com>
2023-01-20 10:41:12 -0400

    cambios realizados, para probar age db

--- schemas/person.py
+++ schemas/person.py
@@ -6,7 +6,8 @@ from typing import Optional
 #Pydantic
 from pydantic import BaseModel
 from pydantic import Field
-class HairColor(Enum):
+
+"""class HairColor(Enum):
     white = "White"
     gray = "Gray"
     brown = "Brown"
@@ -16,8 +17,9 @@ class Location(BaseModel):
     city: str
     state: str
     country: str
-
+"""
 class PersonBase(BaseModel):
+    id: Optional[str]
     first_name: str = Field(
         ...,
         min_length=0,
@@ -28,23 +30,21 @@ class PersonBase(BaseModel):
         min_length=0,
         max_length=50
     )
-    age: int = Field(
-        ...,
-        gt = 0,
-        le = 50
-    )
-    hair_color: Optional[HairColor] = Field(default = None)
-    is_married: Optional[bool] = Field(default = None)
+    age: str 
+#    hair_color: Optional[HairColor] = Field(default = None)
+#    is_married: Optional[bool] = Field(default = None)
 
 class PersonOut(PersonBase):
     pass
 
 class Person(PersonBase):
+    pass
+    """
     password: str = Field(
         ...,
         min_length= 8
     )
-    
+    """
 class LoginOut(BaseModel):
     username: str = Field(...,max_length=20, example="miguel2021")
     message: str = Field(default="Loginsucccesfully")
