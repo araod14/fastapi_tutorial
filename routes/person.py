9716abcca16836d9ea6ea68766f3375d2a8e1647
Daniel Arao <araodaniel14@gmail.com>
2023-01-23 15:16:09 -0400

    con get id funcionando

--- routes/person.py
+++ routes/person.py
@@ -29,28 +29,21 @@ def create_person(person: Person = Body(...)):
     result = conn.execute(persons.insert().values(new_person))
     return conn.execute(persons.select().where(persons.c.id==result.inserted_primary_key['id'])).first()
 
-"""
+
 # Validations: query parameters
 @person.get(
     path = "/person/detail",
     status_code = status.HTTP_200_OK
     )
 def show_person(
-    name: Optional[str] = Query(
-        None,
-        min_length=1, 
-        max_length=50,
-        title = "Person name ",
-        description = "This is the person name. It's between 1 and 50 charapter"
-    ),
-    age: int = Query(
+    id: int = Query(
         ...,
-        title = "Person age",
-        description = "This is the person age. It's required"
+        title = "Person id",
+        description = "This is the person id. It's required"
     )
     ):
-    return {name: age}
-
+    return conn.execute(persons.select().where(persons.c.id == id)).first()
+"""
 @person.get(
     path = "/person/detail/{person_id}",
     status_code = status.HTTP_200_OK
